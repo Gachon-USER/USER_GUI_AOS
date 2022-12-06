efdad15eecbfbc4893577ca97fa21dee2c4c4c5c
hogishim <harrythegreat@gachon.ac.kr>
2022-12-06 23:19:51 +0900

    gui_

--- app/src/main/java/com/example/user/TestAgain.java
+++ app/src/main/java/com/example/user/TestAgain.java
@@ -1,5 +1,6 @@
 package com.example.user;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,15 +8,95 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 
 public class TestAgain extends Fragment {
+
+
+    MainActivity mainActivity;
+    // 메인 액티비티 위에 올린다.
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        mainActivity = (MainActivity) getActivity();
+    }
+
+    // 메인 액티비티에서 내려온다.
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mainActivity = null;
+    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test_again, container, false);
+
+
+
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_test_again, container, false);
+
+
+        ImageButton button6 = rootView.findViewById(R.id.toList);
+
+        button6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.fragmentChange(2);
+            }
+
+        });
+
+        ImageButton button7 = rootView.findViewById(R.id.toM);
+
+        button7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.fragmentChange(4);
+            }
+
+        });
+
+        ImageButton button8 = rootView.findViewById(R.id.toH);
+
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.fragmentChange(1);
+            }
+
+        });
+
+        ImageButton button9 = rootView.findViewById(R.id.toT);
+
+        button9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.fragmentChange(3);
+            }
+
+        });
+
+
+        ImageButton button10 = rootView.findViewById(R.id.toE);
+
+        button10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.fragmentChange(5);
+
+            }
+
+        });
+
+        return rootView;
+
+
+
+
+
     }
 }
